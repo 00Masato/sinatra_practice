fca7d0f2e5079441d18b2362844a8e9cf971b66f
Masato Kato <masato.kato0604@gmail.com>
2020-11-03 17:08:13 +0900

    create api

--- app.rb
+++ app.rb
@@ -1,8 +1,29 @@
 require 'sinatra'
+require 'sinatra/activerecord'
+require './models/balance.rb'
 require 'json'
 
-get '/' do
-  content_type :json
-  data = { foo: 'bar' }
-  data.to_json
+get '/balances' do
+  labels = []
+  data = []
+  balances = Balance.all
+  balances.each do |balance|
+    labels.push(balance.month)
+    data.push(balance.amount)
+  end
+  labels.to_json
+end
+
+get '/balances/:id' do
+  Balance.find(params[:id]).to_json
+end
+
+post '/balances' do
+  json = JSON.parse(request.body.read)
+  balance = Balance.new(json)
+  if balance.save
+    { result: 'success', code: 200 }.to_json
+  else
+    { result: 'failure' }.to_json
+  end
 end
